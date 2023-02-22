80e802d640b921f523ef4e3f79eabd24d31731ac
anthony blalock <anthonyblalock@anthonys-MacBook-Air.local>
2023-02-22 09:55:30 -0500

    I answered the questions

--- tell-us-about-yourself.js
+++ tell-us-about-yourself.js
@@ -1,11 +1,12 @@
 // Tell us about yourself 
 
 // 1. Where do you call home? 
-
+Hughesville, MD
 // 2. Have you lived anywhere else? If so where? 
-
+South Carolina, Connecticut, Hawaii
 // 3. What was your most recent job? What did you enjoy about that role?
-
+Plumber/construction. Fun usuung tools to build things
 // 4. What is your "comfort" food? 
-
+pizza all the way
 // 5. What is one thing you find interesting about yourself ( that others might not notice at first) ? 
+may not look like it but i have a happy demeanor
